dd5f877f15652743c50ac3c084345866f8a829fd
Michael-coding06 <vietkhoaktk@gmail.com>
2026-01-30 00:47:56 +0800

    <feature> change bot to NamKyBot

---
+++ src/main/java/NamKyBot.java
@@ -0,0 +1,6 @@
+public class NamKyBot {
+    public static void main(String[] args) {
+        System.out.println("Hello from NamKyBot\n" );
+        System.out.println("Hope to see you again");
+    }
+}
